8049988f3b24658e5b14dcb860b8d8827b1a2e68
newreport <haruki959@qq.com>
2022-02-20 00:17:55 +0800

    二分查找

---
+++ Base_SQL/FrmMain.cs
@@ -0,0 +1,19 @@
+using System.Data;
+
+namespace Base_SQL
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        void LinkMySQL()
+        {
+
+            string conString = "Data Source={0};Initial Catalog={1};Integrated Security=false;User ID=sa;Password={3}";
+            IDbConnection con = con = new System.Data.SqlClient.SqlConnection(connStr);
+        }
+    }
+}
